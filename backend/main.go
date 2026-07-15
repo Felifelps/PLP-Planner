6b67cf9f745520364c90102b9587294aaad1106c
Felifelps <felifelps.dev@gmail.com>
2026-07-15 16:16:51 -0300

    chore: docker compose setup

--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func ping(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Servidor Ativo")
 }
 
 func main() {
 	mux := http.NewServeMux()
 	
-	mux.HandleFunc("GET /", ping)
+	mux.HandleFunc("GET /", hello)
 
 	println("Servidor rodando em http://localhost:8080")
 	http.ListenAndServe(":8080", mux)
