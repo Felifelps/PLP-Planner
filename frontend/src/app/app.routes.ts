3782044d5f73f0ee85cdc539302b29ca40a6d8f0
Rafael <carvalhorafael527@gmail.com>
2026-08-29 19:56:04 -0300

    feat: tela inicial home e rotas de tarefa e home nas rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'metas', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { 
+    path: 'home', 
+    loadComponent: () => import('./features/home/home-component').then(m => m.HomeComponent) 
+  },
+  {
+   path: 'tarefas',
+   title: 'Tarefas de Hoje - PLP Planner',
+   loadComponent: () =>
+   import('./features/tarefas/tarefas-diarias').then((m) => m.TarefasDiarias),  },
   {
     path: 'metas',
     loadComponent: () => import('./features/metas/metas-list/metas-list').then((m) => m.MetasList),
