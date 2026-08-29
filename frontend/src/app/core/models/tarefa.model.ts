7bf80fd5d03ff16e384b59d45d706ebc9869b586
Rafael <carvalhorafael527@gmail.com>
2026-08-29 19:54:29 -0300

    feat: implementação da tela de serviços diarios

---
+++ frontend/src/app/core/models/tarefa.model.ts
@@ -0,0 +1,19 @@
+export type StatusTarefa = 'pendente' | 'executada' | 'parcialmente executada' | 'cancelada' | 'adiada';
+export type PrioridadeTarefa = 'baixa' | 'média' | 'alta';
+export type DuracaoTarefa = '30min' | '1h';
+export type TurnoTarefa = 'manhã' | 'tarde' | 'noite';
+
+
+export interface Tarefa {
+  id: number;
+  descricao: string;
+  categoria_id: number;
+  data: string; // Formato YYYY-MM-DD
+  horario_inicio?: string; // Formato HH:MM
+  duracao?: DuracaoTarefa;
+  turno?: TurnoTarefa;
+  status: StatusTarefa;
+  prioridade: PrioridadeTarefa;
+}
+
+export type TarefaPayload = Omit<Tarefa, 'id'>;
